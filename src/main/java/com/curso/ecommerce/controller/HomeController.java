b1477213564468c819a3443c080887021da0ac0f
dirceoarce <dirarce2@gmail.com>
2023-09-14 10:30:51 -0500

    Crear funcionalidad ir al carrito desde encabezado template_user, validación doble producto

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -73,19 +73,20 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//validar que el producto no se añada dos veces
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
 		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
+				
 		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		
 		orden.setTotal(sumaTotal);
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
-		
-		
-		
-		
-		
-		
+			
 		return "user/carrito";
 	}
 	
@@ -109,6 +110,15 @@ public class HomeController {
 		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		
 		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "user/carrito";
+	}
+	
+	@GetMapping ("/getCart")
+	public String getCart(Model model) {
+		
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
 		
